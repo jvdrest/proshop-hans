71a1cc1e91599b77ce6c39a9d631572d973c709c
jvdrest <jvdrest@hotmail.com>
2023-11-09 11:37:14 +0100

    Qty & Add To Cart Handler

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -20,7 +20,7 @@ const productSchema = new mongoose.Schema({
   numReviews: { type: Number, required: true, default: 0, },
   price: { type: Number, required: true, default: 0, },
   countInStock: { type: Number, required: true, default: 0, },
-  }, { timestamps: true, }
+}, { timestamps: true, }
 )
 
 const Product = mongoose.model("Product", productSchema)
