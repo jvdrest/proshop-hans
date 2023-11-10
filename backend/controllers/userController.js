5ad453c8980ca9a9e28e28afafe916f16759f594
jvdrest <jvdrest@hotmail.com>
2023-11-10 16:13:32 +0100

    User Email & Password Validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,16 @@ import User from '../models/userModel.js'
 // @access   Public
 //     ipv try-catch blok gebruik van functie asyncHandler
 const authUser = asyncHandler(async (req, res) => {
-  res.send('auth user')
+  const { email, password } = req.body
+  
+  const user = await User.findOne({ email })
+  
+  if (user && (await user.matchPassword(password))) {
+    res.json({_id: user._id, name: user.name, email: user.email, isAdmin: user.isAdmin })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
 })
 
 // @desc     Register user
