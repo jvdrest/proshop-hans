a84f8d676bc4239d7aff267ad36817459688ef19
jvdrest <jvdrest@hotmail.com>
2023-11-15 15:01:48 +0100

    Login Functionality

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -1,20 +1,50 @@
-import { useState } from "react"
-import { Link } from 'react-router-dom'
+import { useState, useEffect } from "react"
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
+import {useDispatch, useSelector} from 'react-redux'
 import FormContainer from '../components/FormContainer'
+import Loader from '../components/Loader'
+import {useLoginMutation} from '../slices/usersApiSlice'
+import { setCredentials } from '../slices/authSlice'
+import {toast} from 'react-toastify'
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-  const submitHandler = (e) => {
+  const [login, { isLoading }] = useLoginMutation()
+  
+  const { userInfo } = useSelector((state) => state.auth)
+
+  const { search } = useLocation()            // als je niet bent ingelogd is waarde van search leeg            
+  const sp = new URLSearchParams(search)      // als search = leeg (size: 0) dan sp = {size: 0} 
+  const redirect = sp.get('redirect') || '/'  // als je bent ingelogd is waarde '/', want redirect is null
+                                              // ben je ingelogd en is redirect aanwezig in params dan bv naar shipping
+  
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect)
+    }
+  }, [userInfo, redirect, navigate])
+
+  const submitHandler = async (e) => {
     e.preventDefault()
-    console.log('submit');
+    try {
+      const res = await login({ email, password }).unwrap()
+      dispatch(setCredentials({ ...res, }))
+      navigate(redirect)
+    } catch (err) {
+      toast.error(err?.data?.message || err.error)
+    }
   }
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
+      console.log(search, sp, redirect);
 
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='email' className='my-3'>
@@ -29,14 +59,16 @@ const LoginScreen = () => {
             type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}>
           </Form.Control>
         </Form.Group>
-        <Button type='submit' variant='primary' className='mt-3'>Sign In</Button>
+        <Button type='submit' variant='primary' className='mt-3' disabled={isLoading}>Sign In</Button>
+
+        {isLoading && <Loader />}
       </Form>
 
       <Row className='py-3'>
         <Col>
-          New Customer? <Link to='/register'>Register</Link>
+          New Customer? <Link to={ redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
         </Col>
-      </Row>
+      </Row> 
     </FormContainer>
   )
 }
